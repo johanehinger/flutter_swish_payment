b5e9e361dcaffd3697efea5f5d62472537d0d91b
Johan Ehinger <65904251+johanehinger@users.noreply.github.com>
2021-12-07 10:16:50 +0100

    Primary Swish Buttons (#16)
    Add primary switch buttons with corresponding doc comment skeleton.

--- lib/flutter_swish_payment.dart
+++ lib/flutter_swish_payment.dart
@@ -8,6 +8,11 @@ enum ButtonTypes {
   elevatedButton,
 }
 
+enum LogoTypes {
+  primary,
+  secondary,
+}
+
 /// # A Material Design "Swish button"
 ///
 /// When adding Swish widgets to the UI, special care should be taken to
@@ -65,6 +70,26 @@ enum ButtonTypes {
 /// > - https://github.com/johanehinger/flutter_swish_payment
 /// > - https://developer.swish.nu/
 class SwishButton extends StatelessWidget {
+  /// Create an elevated button with the primary logo of Swish.
+  ///
+  const SwishButton.primaryElevatedButton({
+    Key? key,
+    required this.onPressed,
+    this.style,
+  })  : buttonType = ButtonTypes.elevatedButton,
+        logo = LogoTypes.primary,
+        super(key: key);
+
+  /// Create an text button with the primary logo of Swish.
+  ///
+  const SwishButton.primaryTextButton({
+    Key? key,
+    required this.onPressed,
+    this.style,
+  })  : buttonType = ButtonTypes.textButton,
+        logo = LogoTypes.primary,
+        super(key: key);
+
   /// Create an elevated button with the secondary logo of Swish.
   ///
   /// ![Swish Secondary Logo](https://github.com/johanehinger/flutter_swish_payment/blob/main/images/swish_logo_secondary_RGB.png?raw=true|width=250,height=75)
@@ -73,6 +98,7 @@ class SwishButton extends StatelessWidget {
     required this.onPressed,
     this.style,
   })  : buttonType = ButtonTypes.elevatedButton,
+        logo = LogoTypes.secondary,
         super(key: key);
 
   /// Create an text button with the secondary logo of Swish.
@@ -83,20 +109,36 @@ class SwishButton extends StatelessWidget {
     required this.onPressed,
     this.style,
   })  : buttonType = ButtonTypes.textButton,
+        logo = LogoTypes.secondary,
         super(key: key);
 
   final ButtonStyle? style;
   final VoidCallback? onPressed;
   final ButtonTypes buttonType;
+  final LogoTypes logo;
 
   @override
   Widget build(BuildContext context) {
-    Image _logo = const Image(
-      image: AssetImage(
-        'images/swish_logo_secondary_RGB.png',
-        package: 'flutter_swish_payment',
-      ),
-    );
+    Image _logo;
+    switch (logo) {
+      case LogoTypes.primary:
+        _logo = const Image(
+          image: AssetImage(
+            'images/swish_logo_primary_RGB.png',
+            package: 'flutter_swish_payment',
+          ),
+        );
+        break;
+      case LogoTypes.secondary:
+        _logo = const Image(
+          fit: BoxFit.fitWidth,
+          image: AssetImage(
+            'images/swish_logo_secondary_RGB.png',
+            package: 'flutter_swish_payment',
+          ),
+        );
+        break;
+    }
 
     ButtonStyle _style = style ??
         ButtonStyle(
@@ -105,6 +147,9 @@ class SwishButton extends StatelessWidget {
           ),
           overlayColor: MaterialStateProperty.all(
             Colors.grey[100],
+          ),
+          padding: MaterialStateProperty.all<EdgeInsets>(
+            const EdgeInsets.all(8.0),
           ),
         );
 
